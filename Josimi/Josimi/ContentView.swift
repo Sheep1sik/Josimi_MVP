54ad1031c55114fdf5172e3ca9e7bef433cf790f
Sheep1sik <bohg6799@naver.com>
2024-06-20 18:04:01 +0900

    Feat: 프로젝트 생성

---
+++ Josimi/Josimi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Josimi
+//
+//  Created by 양원식 on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Josimi/Josimi/JosimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JosimiApp.swift
+//  Josimi
+//
+//  Created by 양원식 on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct JosimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
